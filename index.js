5ed3f99e2ad076435181d73e8e8abb4cf37fc6b8
Keith Lazuka <kelazuka@microsoft.com>
2016-10-13 16:18:25 -0700

    Workaround for error when unmounting the component
    This is a temporary workaround for #3, which happens anytime
    the React component is unmounted (e.g. by a router when switching
    to a different route).

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var React = require('react');
 
 module.exports = React.createClass({
-	initialize: function(node) {
-		var app = this.props.src.embed(node, this.props.flags);
-
-		if (typeof this.props.ports !== 'undefined') {
-			this.props.ports(app.ports);
-		}
+	storeRef: function(node) {
+		this.myRef = node;
 	},
 
 	shouldComponentUpdate: function(prevProps) {
 		return false;
 	},
 
-	render: function () {
-		return React.createElement('div', { ref: this.initialize });
+	render: function() {
+		return React.createElement('div', { ref: this.storeRef });
+	},
+
+	componentDidMount: function() {
+		// Workaround issue #3 by embedding the Elm module every time the
+		// component mounts. This is definitely not the way 'embed' is meant
+		// to be used, but it seems to work. One thing I did notice is that
+		// it appears to leak memory, but the good news is that Chrome GC
+		// eventually catches up. Nevertheless, we should fix as this
+		// workaround *may* result in nasty bugs.
+		// TODO [kl] improve the workaround by embedding the Elm app only once!
+		var app = this.props.src.embed(this.myRef, this.props.flags);
+
+		if (typeof this.props.ports !== 'undefined') {
+			this.props.ports(app.ports);
+		}
+	},
+
+	componentWillUnmount: function() {
+		// Ideally we would stop Elm here, but there is no such API currently.
 	}
 });
